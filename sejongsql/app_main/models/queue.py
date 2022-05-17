13f4dc2caf2863ee4ded0ba78a5fd693ce857316
akshxl09 <59478929+akshxl09@users.noreply.github.com>
2022-05-17 20:47:34 +0900

    update: queue default값 수정

--- sejongsql/app_main/models/queue.py
+++ sejongsql/app_main/models/queue.py
@@ -4,7 +4,7 @@ class Queue(models.Model):
     user_id = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
     type_id = models.IntegerField()
-    status = models.CharField(max_length=100, default='작업중')
+    status = models.CharField(max_length=100, default='working')
     created_at = models.DateTimeField(auto_now_add=True)
     class Meta:
         db_table = 'ssql_queue'
