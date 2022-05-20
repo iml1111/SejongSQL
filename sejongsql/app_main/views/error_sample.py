ff700f433c9d3709817ebb5d384e9f569699d532
Taebo Won <59478929+akshxl09@users.noreply.github.com>
2022-05-20 16:29:47 +0900

    [feat-57] 비동기 큐 테스트 API 생성 (#58)
    * feat: 비동기 예제 추가
    
    * feat: 비동기 예제 추가

--- sejongsql/app_main/views/error_sample.py
+++ sejongsql/app_main/views/error_sample.py
@@ -1,6 +1,25 @@
+from django.conf import settings
 from rest_framework.views import APIView
-
+from module.async_queue import get_async_queue, freeze
+from module.response import OK
+import time
 
 class ErrorSample(APIView):
     def get(self, request):
         raise KeyError("500 SERVER ERROR TEST")
+
+
+class AsyncTestView(APIView):
+    def get(self, request):
+        
+        q = get_async_queue(
+            worker_num=getattr(settings, 'ASYNC_QUEUE_WORKER', None),
+            qsize=getattr(settings, 'ASYNC_QUEUE_SIZE', None),
+        )
+        q.add(freeze(hello_world)())
+        return OK()
+
+
+def hello_world():
+    time.sleep(5)
+    print("Hello World!!!!!")
